a3b35276de69b449361c587abb37ad38115a9109
jjyeon <wodus9193@naver.com>
2024-03-31 10:24:32 +0900

    신규 생성 시작

---
+++ src/main/java/org/example/onlinecoffeeordersystem/OnlineCoffeeOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinecoffeeordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoffeeOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCoffeeOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/onlinecoffeeordersystem/OnlineCoffeeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinecoffeeordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoffeeOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
